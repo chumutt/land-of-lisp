0eb059c0dd7d24ec47fd7edde919c92bc33ba8d9
ChuMutt <chufilthymutt@gmail.com>
2024-02-28 23:53:10 -0600

    init

---
+++ ch7/graph-util.lisp
@@ -0,0 +1,23 @@
+(defparameter *max-label-length* 28)
+
+(defun dot-name (exp)
+  (substitute-if #\_ (complement #'aplhanumericp) (prin1-to-string exp)))
+
+(defun dot-label (exp)
+  (if exp
+      (let ((s (write-to-string exp :pretty nil)))
+        (if (> (length s) *max-label-length*)
+            (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+            s))
+      ""))
+
+(defun nodes->dot (nodes)
+  (mapc (lambda (node)
+          (fresh-line)
+          (princ (dot-name (car node)))
+          (princ "[label=\"")
+          (princ (dot-label node))
+          (princ "\"];"))
+        nodes))
+
+(defun edges->dot (edges) ...)
